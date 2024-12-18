53b51362d60511ecb789e24ec3a099b39a9c3f99
Nikola Stojanovic <68916411+dzonidoo@users.noreply.github.com>
2024-12-18 14:57:05 +0100

    Improve the config options for the fields shown in the Publish and Send to Panel (#4703)

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -3417,6 +3417,16 @@ declare module 'superdesk-api' {
             preview?: {
                 hideContentLabels: boolean;
             };
+
+            panels?: {
+                publish?: {
+                    publishSchedule?: boolean
+                    publishingTarget?: boolean;
+                }
+                sendTo?: {
+                    publishSchedule?: boolean;
+                }
+            }
         };
         ui: {
             /**
